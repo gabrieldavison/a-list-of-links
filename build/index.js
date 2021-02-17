777c8a67ff8ee896c7ae1b9341fa998b0e1124bf
Gabriel <mail@gabrieldavison.me.uk>
2021-02-17 22:52:45 +0000

    Read data from pinboard api and store locally

--- build/index.js
+++ build/index.js
@@ -4,10 +4,48 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-// rest of the code remains same
+var https_1 = __importDefault(require("https"));
+var fs_1 = __importDefault(require("fs"));
+var path_1 = __importDefault(require("path"));
 var app = express_1.default();
 var PORT = 8000;
-app.get("/", function (req, res) { return res.send("Express + TypeScript Server"); });
+app.get("/", function (req, res) { return res.send("get root"); });
+app.get("/update", function (req, res) {
+    var properURL = "https://api.pinboard.in/v1/posts/all?format=json&tag=shareList&auth_token=" + process.env.PINBOARD_TOKEN;
+    var testURL = "https://api.pinboard.in/v1/posts/recent?format=json&auth_token=" + process.env.PINBOARD_TOKEN;
+    https_1.default
+        .get(testURL, function (resp) {
+        var body = "";
+        // A chunk of data has been received.
+        resp.on("data", function (chunk) {
+            body += chunk;
+            process.stdout.write(chunk);
+            // console.log("chunk");
+        });
+        // The whole response has been received. Print out the result.
+        resp.on("end", function () {
+            console.log("end", body);
+            try {
+                var jsonData = JSON.parse(body);
+                console.log(jsonData);
+                var filePath = path_1.default.join(__dirname, "data", "data.json");
+                fs_1.default.writeFile(filePath, jsonData, function (err) {
+                    if (err) {
+                        return res.send(err);
+                    }
+                    res.send("success");
+                });
+            }
+            catch (e) {
+                console.log("Error parsing JSON!");
+                console.log(body);
+            }
+        });
+    })
+        .on("error", function (err) {
+        res.send("Error: " + err.message);
+    });
+});
 app.listen(PORT, function () {
-    console.log("\u26A1\uFE0F[server]: Server is running at https://localhost:" + PORT);
+    console.log("\u26A1\uFE0F[server]: Server is running at http://localhost:" + PORT);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,43 @@
 import express from "express";
+import dotenv from "dotenv";
+import https from "https";
+import { promises as fs } from "fs";
+import path from "path";
 
-// rest of the code remains same
+dotenv.config();
 const app = express();
 const PORT = 8000;
-app.get("/", (req, res) => res.send("Hello world"));
+
+app.get("/", async (req, res) => {
+  const filePath = path.join(__dirname, "data", "data.json");
+  const jsonData = await fs.readFile(filePath, "utf8");
+  const linkData = await JSON.parse(jsonData);
+  console.log(linkData);
+});
+
+app.get("/update", (req, res) => {
+  const reqURL = `https://api.pinboard.in/v1/posts/all?format=json&tag=shareList&auth_token=${process.env.PINBOARD_TOKEN}`;
+  https
+    .get(reqURL, (resp) => {
+      let body = "";
+      resp.on("data", (chunk) => {
+        body += chunk;
+      });
+      resp.on("end", () => {
+        try {
+          const filePath = path.join(__dirname, "data", "data.json");
+          fs.writeFile(filePath, body);
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    })
+    .on("error", (err) => {
+      res.send("Error: " + err.message);
+    });
+});
+
 app.listen(PORT, () => {
+  console.log(process.env.PINBOARD_TOKEN);
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
